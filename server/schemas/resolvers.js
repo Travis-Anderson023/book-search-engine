fe29f6d49350e53cc0ca000c73fd910fcca17ef9
Spotexx <tsanderson.023@gmail.com>
2022-03-25 19:09:44 -0400

    for test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,13 @@
 const { Book, User } = require('../models');
 
+// createUser,
+//   getSingleUser,
+//   saveBook,
+//   deleteBook,
+//   login,
+
 const resolvers = {
+
 }
 
 module.exports = resolvers;
